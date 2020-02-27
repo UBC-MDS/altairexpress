2c6f897e2aa7618705edda038e1d5e7532c7e508
tphaterp <tphaterp@gmail.com>
2020-02-27 07:59:12 -0800

    added basic docstring

---
+++ altairexpress/scatter.py
@@ -0,0 +1,13 @@
+def make_scatter(df, xval = 'age',yval = 'trestbps', x_transform = None, y_transform = None):
+    '''
+    Takes in user-specified dataframe and creates a scatterplot that compares two numerical features/variables.
+
+    Arguements:
+
+    df - (dataframe) Dataframe 
+    xval -
+    yval -
+    x_transform
+    y_transform
+    '''
+    return
