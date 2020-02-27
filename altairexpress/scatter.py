488ed31a29c6f98535c0156316b7cc690e00bed8
tphaterp <tphaterp@gmail.com>
2020-02-27 09:16:08 -0800

    added more details for the docstring

--- altairexpress/scatter.py
+++ altairexpress/scatter.py
@@ -1,13 +1,14 @@
-def make_scatter(df, xval = 'age',yval = 'trestbps', x_transform = None, y_transform = None):
+def make_scatter(df, xval = None, yval = None, x_transform = False, y_transform = True):
     '''
-    Takes in user-specified dataframe and creates a scatterplot that compares two numerical features/variables.
+    Takes in a dataframe and creates a scatterplot that compares two numerical features/variables that are specified by the user.
+    Additionally, the user can toggle natural log transformations on both the x and y axis.
 
     Arguements:
 
     df - (dataframe) Dataframe 
-    xval -
-    yval -
-    x_transform
-    y_transform
+    xval - (String) variable/column name 
+    yval - (String) variable/column name
+    x_logtransform - (Boolean) determines whether a natural log transformation occurs on the x-axis
+    y_logtransform - (Boolean) determines whether a natural log transformation occurs on the y-axis
     '''
     return
