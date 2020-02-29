03608a6c08d2d368478b759022f08ee6bd07fd28
thejacktan <59751623+thejacktan@users.noreply.github.com>
2020-02-29 10:56:20 -0800

    Added Fourier transform function docstring

---
+++ altairexpress/fourier_transform.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+
+def fourier_transform(data, time_col, data_col):
+    '''
+    Creates fourier transform plot of the data specified in the 'time_col' column
+
+    Parameters
+    ----------
+    data: dataframe
+        pandas dataframe containing all neccessary information
+    time_col: str
+        string containing the time data column corresponding to the data we
+        want to apply fourier transform to
+    data_col: str
+        string containing the name of the column we want to apply fourier transform to
+
+
+    Returns
+    -------
+    altair plot
+        altair plot of frequency vs amplitude of the data inputed
+
+
+    Examples
+    --------
+    >>> my_data = pd.DataFrame(data = {'time_series': [0, 1, 2, 3],
+                                       'signal': [2, 3, 4, 6]})
+    >>> altairexpress.fourier_transform(data = my_data,
+                                        time_col = 'time_series',
+                                        data_col = 'signal')
+
+    '''
