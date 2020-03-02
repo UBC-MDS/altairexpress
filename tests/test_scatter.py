8f6b2cc691732641284556d6518cfba3e074bd4d
tphaterp <tphaterp@gmail.com>
2020-03-02 15:49:32 -0800

    added some test ideas

--- tests/test_scatter.py
+++ tests/test_scatter.py
@@ -1,2 +1,7 @@
 # test_scatter.py
 
+def scatter_unit_tests():
+    # check axis; are the variables numeric
+    # check axis of the plot itself
+    # check the mark
+    return (print("nothing yet"))
