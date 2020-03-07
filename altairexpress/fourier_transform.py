3cc691d41e398c6c1739ba92dcfebd199ecbd678
thejacktan <59751623+thejacktan@users.noreply.github.com>
2020-03-07 11:26:37 -0800

    Edits to assert statements

--- altairexpress/fourier_transform.py
+++ altairexpress/fourier_transform.py
@@ -47,7 +47,7 @@ def fourier_transform(data, time_col, data_col):
     assert((data[data_col] == data[data_col].dropna()).all()), \
     '`NaN` values found in signal column! Please Fill!'
     assert((data[time_col] == data[time_col].dropna()).all()), \
-    '`NaN` values found in time column!'
+    '`NaN` values found in time column! Please Fill!'
 
     for n in range(len(my_time) - 1):
         assert np.isclose(my_time[n + 1] - my_time[n], sampling_freq), \
