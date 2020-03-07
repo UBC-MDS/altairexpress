714de49e7901635e21072baa0166860c0f29499d
thejacktan <59751623+thejacktan@users.noreply.github.com>
2020-03-07 15:33:56 -0800

    Updated test descriptions

--- tests/test_fourier.py
+++ tests/test_fourier.py
@@ -23,22 +23,22 @@ def test_fourier():
     my_df1 = pd.DataFrame(data = {'X1': x, 'X2': y1})
     my_df2 = pd.DataFrame(data = {'X1': x, 'X2': y2})
     
-    # Test 1
+    # Test 1: check the type of the output
     assert isinstance(ft.fourier_transform(data = my_df1, time_col = 'X1', data_col = 'X2'), \
     alt.vegalite.v3.api.Chart), "Return type is not an Altair object!"
     assert isinstance(ft.fourier_transform(data = my_df2, time_col = 'X1', data_col = 'X2'), \
     alt.vegalite.v3.api.Chart), "Return type is not an Altair object!"
-    # Test 2
+    # Test 2: check if the output is a line chart
     assert ft.fourier_transform(data = my_df1, time_col = 'X1', data_col = 'X2') == "line", \
     "Return plot should be a line plot!"
     assert ft.fourier_transform(data = my_df2, time_col = 'X1', data_col = 'X2') == "line", \
     "Return plot should be a line plot!"
-    # Test 3
+    # Test 3: check the data type of the x-axis mapping
     assert ft.fourier_transform(my_df1, 'X1', 'X2').to_dict()['encoding']['x']['type'] == "quantitative", \
     "Plot x-axis should be of quantitative type!"
     assert ft.fourier_transform(my_df2, 'X1', 'X2').to_dict()['encoding']['x']['type'] == "quantitative", \
     "Plot x-axis should be of quantitative type!"
-    # Test 4
+    # Test 4: check the data type of the y-axis mapping
     assert ft.fourier_transform(my_df1, 'X1', 'X2').to_dict()['encoding']['y']['type'] == "quantitative", \
     "Plot y-axis should be of quantitative type!"
     assert ft.fourier_transform(my_df2, 'X1', 'X2').to_dict()['encoding']['y']['type'] == "quantitative", \
