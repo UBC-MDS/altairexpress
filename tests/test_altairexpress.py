aac8a724cc998666be79e9c76b7ec0e45fd7c15b
Lesley <iamthelm@gmail.com>
2020-03-13 10:08:02 -0700

    committing edits before merge with master

--- tests/test_altairexpress.py
+++ tests/test_altairexpress.py
@@ -2,9 +2,10 @@ from altairexpress import altairexpress as ax
 import pytest
 # import pandas as pd
 # import numpy as np
-# import altair as alt
+import altair as alt
 
 
+@pytest.fixture
 def test_ts():
     """
     Tests the time series function ts_alt to make sure the outputs are
@@ -15,18 +16,18 @@ def test_ts():
         The test should pass and no asserts should be displayed.
     """
     test_df1 = "https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-\
-mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=\
-AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM"
+        mod_students/master/labs/release/lab1/data/ts4_jjshares.csv?token=\
+            AAAAOMZQBS5IDU4Q6K3RBXS6NG7NM"
     test_df2 = "https://raw.github.ubc.ca/MDS-2019-20/DSCI_574_spat-temp-\
-mod_students/master/labs/release/lab1/data/ts1_globaltemp.csv?token=\
-AAAAOM4GFOSICJVSVP2YUP26NG7QE"
+        mod_students/master/labs/release/lab1/data/ts1_globaltemp.csv?token=\
+            AAAAOM4GFOSICJVSVP2YUP26NG7QE"
     # test the plot type is correct
-    # assert isinstance(ax.ts_alt(test_df1, "earnings", 4),
-    #                   alt.vegalite.v3.api.Chart), \
-    #     "Plot type is not an Altair object."
-    # assert isinstance(ax.ts_alt(test_df2, "temp", 1),
-    #                   alt.vegalite.v3.api.Chart), \
-    #     "Plot type is not an Altair object."
+    assert isinstance(ax.ts_alt(test_df1, "earnings", 4),
+                      alt.vegalite.v3.api.Chart), \
+        "Plot type is not an Altair object."
+    assert isinstance(ax.ts_alt(test_df2, "temp", 1),
+                      alt.vegalite.v3.api.Chart), \
+        "Plot type is not an Altair object."
     assert ax.ts_alt(test_df1, "earnings", 4).to_dict()[
                      'mark'] == "line", "The plot should be line charts."
     assert ax.ts_alt(test_df2, "temp", 1).to_dict()[
@@ -51,7 +52,7 @@ AAAAOM4GFOSICJVSVP2YUP26NG7QE"
         assert ax.ts_alt(123, "earnings", 4)
     assert str(
         e.value) == "TypeError: The path of the data must be entered as a \
-string."
+            string."
 
     with pytest.raises(Exception) as e:
         assert ax.ts_alt(test_df1, 2, 4)
@@ -62,16 +63,16 @@ string."
         assert ax.ts_alt(test_df1, "earnings", "4")
     assert str(
         e.value) == "ValueError: Frequency must be an integer from {1, 4, 12, \
-52}."
+            52}."
 
     with pytest.raises(Exception) as e:
         assert ax.ts_alt(test_df1, "earnings", 6)
     assert str(
         e.value) == "ValueError: Frequency must be an integer from {1, 4, 12, \
-52}."
+            52}."
 
     with pytest.raises(Exception) as e:
         assert ax.ts_alt(test_df1, "earning", 4)
     assert str(
         e.value) == "ValueError: The column name were not found in the \
-original data."
+            original data."
